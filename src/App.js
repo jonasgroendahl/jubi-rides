e89c917a3431dd50f42065de0d5c1618ba8fb4ae
Jonas <jonas.groendahlxd@gmail.com>
2019-09-25 13:49:24 +0200

    add danish

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import TopNavigation from "./components/TopNavigation";
 import { MuiThemeProvider, Grid, Typography, List, ListItem, ListItemText, ListItemIcon } from "@material-ui/core";
 import { DirectionsCar, Smartphone, Money } from "@material-ui/icons";
 import theme from "./utils/theme";
+import LangFile from "./utils/LangFile";
 
 import requestRideImg from "./assets/phone-mockup-request-ride.png";
 import stores from "./assets/stores.png";
 
 function App() {
+  const [lang, setLang] = useState("uk");
+
+  const $ = LangFile[lang];
+
   return (
     <MuiThemeProvider theme={theme}>
       <div className="App">
-        <TopNavigation />
+        <TopNavigation lang={lang} onChange={setLang} $={$} />
         <main>
           <Grid container>
             <Grid item sm={6} xs={12}>
@@ -19,34 +24,36 @@ function App() {
             </Grid>
             <Grid item sm={6} xs={12} style={{ marginTop: 30 }}>
               <Typography variant="h5" style={{ fontWeight: "bold" }} gutterBottom>
-                Tired of all the drawbacks of the Danish taxi system but need a ride?
+                {$.headline}
               </Typography>
-              <Typography variant="body2">With Jubi Rides you are guaranteed:</Typography>
+              <Typography variant="body2">{$.listHeader}</Typography>
               <List>
                 <ListItem>
                   <ListItemIcon>
                     <DirectionsCar />
                   </ListItemIcon>
-                  <ListItemText primary="Experienced and skilled drivers." secondary="Only riders with past experience are allowed to sign-up." />
+                  <ListItemText primary={$.listItem1p} secondary={$.listItem1s} />
                 </ListItem>
                 <ListItem>
                   <ListItemIcon>
                     <Smartphone />
                   </ListItemIcon>
-                  <ListItemText primary="Convenient." secondary="Look for rides using the mobile App." />
+                  <ListItemText primary={$.listItem2p} secondary={$.listItem2s} />
                 </ListItem>
                 <ListItem>
                   <ListItemIcon>
                     <Money />
                   </ListItemIcon>
-                  <ListItemText primary="Low cost. Affordable." secondary="Know the final price when selecting your ride." />
+                  <ListItemText primary={$.listItem3p} secondary={$.listItem3s} />
                 </ListItem>
               </List>
               <Typography variant="body1" style={{ marginTop: 20 }}>
-                Soon available in a city near you in Denmark and in the Google Play Store and Apple App store.
+                {$.soon}
               </Typography>
               <img src={stores} alt="Google play and apple app store" height={50} style={{ marginTop: 15, display: "block" }} />
-              <Typography variant="caption">Predicted launch 2020 Q1</Typography>
+              <Typography variant="subtitle1" style={{ marginTop: 10 }}>
+                {$.eta}
+              </Typography>
             </Grid>
           </Grid>
         </main>
--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -1,42 +1,78 @@
 import React, { useState } from "react";
-import { AppBar, Toolbar, Button, Dialog, Typography, List, ListItem, ListItemText, DialogContent, DialogActions } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Dialog,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  DialogContent,
+  DialogActions,
+  Menu,
+  MenuItem
+} from "@material-ui/core";
 import { Logo } from "../utils/vars";
 
-export default function TopNavigation() {
+import dk from "../assets/flags/dk.svg";
+import uk from "../assets/flags/uk.svg";
+
+const FLAG_HEIGHT = 30;
+
+export default function TopNavigation({ lang = "uk", onChange, $ }) {
   const [isOpen, setOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  function handleClick(langCode) {
+    onChange(langCode);
+    setAnchorEl(null);
+  }
+
   return (
     <AppBar color="default" position="sticky" style={{ backgroundColor: "#e1f0f0" }}>
       <Toolbar>
         <img src={Logo} height={55} alt="" />
         <div style={{ marginLeft: "auto" }}>
-          <Button onClick={() => setOpen(true)}>Sign up to become a driver</Button>
+          <Button onClick={e => setAnchorEl(e.currentTarget)}>
+            <img src={lang === "dk" ? dk : uk} height={FLAG_HEIGHT} alt={lang === "dk" ? "Danish" : "English"} />
+          </Button>
+          <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={() => setAnchorEl(null)}>
+            <MenuItem onClick={() => handleClick("uk")}>
+              <img src={uk} height={FLAG_HEIGHT} alt="English" />
+            </MenuItem>
+            <MenuItem onClick={() => handleClick("dk")}>
+              <img src={dk} height={FLAG_HEIGHT} alt="Danish" />
+            </MenuItem>
+          </Menu>
+          <Button onClick={() => setOpen(true)}>{$.topNavBtn}</Button>
         </div>
       </Toolbar>
       <Dialog open={isOpen} onClose={() => setOpen(false)}>
         <DialogContent>
           <Typography variant="body1">
-            Send the following information to <span style={{ fontWeight: 700 }}>jonas.groendahlxd@gmail.com</span>:
+            {$.dialogTitle} <span style={{ fontWeight: 700 }}>jonas.groendahlxd@gmail.com</span>:
           </Typography>
           <List>
             <ListItem>
-              <ListItemText primary="Personal information" secondary="Name, address, year born, etc." />
+              <ListItemText primary={$.dialogListItem1p} secondary={$.dialogListItem1s} />
             </ListItem>
             <ListItem>
-              <ListItemText primary="All relevant driving experience" secondary="Assets that prove your skills." />
+              <ListItemText primary={$.dialogListItem2p} secondary={$.dialogListItem2s} />
             </ListItem>
             <ListItem>
-              <ListItemText primary="Cover letter" secondary="Explanation of why you'd like to become a driver at Jubi" />
+              <ListItemText primary={$.dialogListItem3p} secondary={$.dialogListItem3s} />
             </ListItem>
             <ListItem>
-              <ListItemText primary="Photo" secondary="A newly shot photograph of you" />
+              <ListItemText primary={$.dialogListItem4p} secondary={$.dialogListItem4s} />
             </ListItem>
             <ListItem>
-              <ListItemText primary="Car information" secondary="All relevant car information such as brand, km rode, color, current state" />
+              <ListItemText primary={$.dialogListItem5p} secondary={$.dialogListItem5s} />
             </ListItem>
           </List>
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => setOpen(false)}>Close this window</Button>
+          <Button onClick={() => setOpen(false)}>{$.dialogBtn}</Button>
         </DialogActions>
       </Dialog>
     </AppBar>
